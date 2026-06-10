785df3a30c8092766d06dff90a275e450fd21bbe
LaoBwait <2918572856@qq.com>
2026-06-10 13:32:38 +0800

    replace node

---
+++ editNode.js
@@ -0,0 +1,36 @@
+const parser = require("@babel/parser");
+const traverse = require("@babel/traverse").default;
+const generator = require("@babel/generator").default;
+const t = require("@babel/types");
+const template = require("@babel/template").default;
+
+const code = `
+var a = 1, b = 2;
+let c = 3;
+const d = 4;
+let e, f, g = function () { console.log('ggg'); };
+g();
+`
+
+const ast = parser.parse(code);
+
+const Myvisitor = {
+    VariableDeclaration(path) {
+        let {kind, declarations} = path.node;
+
+        if (declarations.length <= 1){
+            return;
+        }
+
+        let VariableDeclarationNodes = [];
+        for( i of declarations ){
+            VariableDeclarationNodes.push(t.variableDeclaration(kind,[i]));
+        }
+
+        path.replaceWithMultiple(VariableDeclarationNodes);
+    }
+}
+
+traverse(ast, Myvisitor);
+const output = generator(ast);
+console.log(output);
